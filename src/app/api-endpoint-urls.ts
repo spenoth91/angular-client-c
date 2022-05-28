b5c3956c680a299b9a20be2d19bacb883364e133
Sikó Márk Levente <markforsapi@gmail.com>
2022-05-28 17:33:07 +0300

    Register and Login added
    !!!You need to install Angular Material and Primeng!!! and also have angular 13 and TypeScript >=4.4.2 and <4.7.0

--- src/app/api-endpoint-urls.ts
+++ src/app/api-endpoint-urls.ts
@@ -4,4 +4,11 @@ export class APIEndpointURLs {
   // User
   public static readonly userUrl = APIEndpointURLs.baseUrl + '/user';
   public static readonly allUser = APIEndpointURLs.userUrl + '/all';
+  public static readonly user = APIEndpointURLs.userUrl + '/id/';
+  public static readonly myStuff = APIEndpointURLs.userUrl + '/stuff';
+
+  // Auth
+  public static readonly authUrl = APIEndpointURLs.baseUrl + '/auth';
+  public static readonly registerUrl = APIEndpointURLs.authUrl + '/register';
+  public static readonly loginUrl = APIEndpointURLs.authUrl + '/login';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { UserListComponent } from './users/components/user-list/user-list.component';
 import {TableModule} from 'primeng/table';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './account/component/login/login.component';
+import { RegisterComponent } from './account/component/register/register.component';
+import { StuffListComponent } from './users/components/stuff-list/stuff-list.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    StuffListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    TableModule
+    TableModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
