433977f52ac330fad0596606cf0bccfd563fac39
Sikó Márk Levente <markforsapi@gmail.com>
2022-06-06 14:36:18 +0300

    Register Page Designed

--- src/app/account/component/register/register.component.ts
+++ src/app/account/component/register/register.component.ts
@@ -4,8 +4,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import {AccountService} from '../services/account.service';
 
-@Component({ templateUrl: 'register.component.html' })
-export class RegisterComponent implements OnInit {
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})export class RegisterComponent implements OnInit {
   form: FormGroup;
   loading = false;
   submitted = false;
